3f83bff5958a2cbe2ab183aa16a243bf1fc92633
temitopejohn <temitopejohn91@gmail.com>
2022-10-17 06:53:08 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,19 @@
-nclude <unistd.h>
+#include <unistd.h>
 
 /**
- * main - program entry point
- * Return: To return value 0
+ * main - entry point
+ * Return: Always 0
  */
+
 int main(void)
+
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		write(2,
 
-		return (1);
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+										59);
+	
+			return (1);
+
 }
-
-
-
-
-
-
-
-
-
-
