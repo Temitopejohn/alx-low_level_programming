a0e4c2da50ceca6115cfe6e7ef8c00a53753ba32
temitopejohn <temitopejohn91@gmail.com>
2022-10-17 05:24:19 -0700

    0-preprocessor

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,23 @@
-#include <unistd.h>
+nclude <unistd.h>
 
 /**
- * main - entry point
- * Return: Always 0
+ * main - program entry point
+ * Return: To return value 0
  */
 int main(void)
 {
-		write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-			  	59);
-		return (1);
 
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+		return (1);
 }
+
+
+
+
+
+
+
+
+
+
