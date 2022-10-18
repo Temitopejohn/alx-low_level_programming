7ef9dc5306b7ac23056a61c4beaa71cb26c2156f
temitopejohn <temitopejohn91@gmail.com>
2022-10-18 09:09:50 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,7 +14,7 @@ for (a = 0; a < 8; a++)
 		for (c = b + 1; c < 10; c++)
 		{
 		       putchar((a % 10) + '0');
-										       		putchar((b % 10) + '0');
+       		putchar((b % 10) + '0');
 		putchar((c % 10) + '0');
 		if (a == 7 && b == 8 && c == 9)
 			continue;
